338f70516973223db0625b426f772442b7457ff6
LT_Ongarj <ongarj2314@gmail.com>
2025-11-29 21:07:58 +0700

    Learning DateTime format

--- week15/TimeV2.js
+++ week15/TimeV2.js
@@ -1,4 +1,21 @@
 export async function formatLocal(dtIso, locale, timeZone, hourCycle){
-    let dateUpdate = new Date("2025-11-30T23:15:30Z");
+    let dateUpdate = new Date(dtIso);
+    let datePart = Intl.DateTimeFormat(locale,{
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        timeZone
+    }).format(dateUpdate)
+    let timePart = Intl.DateTimeFormat(locale,{
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hourCycle,
+        timeZone
+    }).format(dateUpdate)
+    console.log(`${datePart}, ${timePart}`)
 
 }
+
+
+formatLocal("2025-11-30T23:15:30Z", "th-TH", "Asia/Bangkok", "h23")
