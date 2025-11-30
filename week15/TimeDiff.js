9cc09498d913419a8e69d923ece7077aa1c6fccf
LT_Ongarj <ongarj2314@gmail.com>
2025-11-30 17:09:47 +0700

    Final FrontEnd Goodbye Explain DiffTime And Format Time with Intl

--- week15/TimeDiff.js
+++ week15/TimeDiff.js
@@ -1,2 +1,12 @@
 //สงสัยว่าเราจะหาความต่างของ เวลาอย่างไร
 
+export function diffTime(dateStart,dateEnd) {
+    const Start = new Date(dateStart);
+    const End = new Date(dateEnd);
+    const Diff = End - Start
+    const diffHours = Diff / (1000 * 60 * 60);
+    const diffDays = diffHours / 24
+    return diffDays
+}
+
+console.info(diffTime("2025-01-01T10:00:00","2025-12-01T10:30:00"));
